2e107ef1bc123d86eef2539ef9f6f9e6a23d8201
pero <githubmail19@gmail.com>
2026-08-18 10:48:11 +0300

    fixing buid errors

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,10 @@
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-// بننشئ الـ Adapter ونديله رابط الاتصال من متغير البيئة
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
 
-// عشان نمنع فتح اتصالات جديدة كل مرة بيحصل فيها Hot Reload في وضع التطوير
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
